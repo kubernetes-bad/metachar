7eab618c7b7d569eae26162344ceb4031287a5e2
kubernetes-bad <titular.jug.0e@icloud.com>
2024-01-08 16:22:07 -0700

    Add local config file override - backend/config/local.yml

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -1,9 +1,21 @@
-import { readFileSync } from 'node:fs';
+import { readFileSync, existsSync } from 'node:fs';
 import { join, resolve } from 'node:path';
 import * as yaml from 'js-yaml';
 
-const YAML_CONFIG_FILENAME = 'default.yml';
+const YAML_DEFAULT_CONFIG_FILENAME = 'default.yml';
+const YAML_OVERRIDE_CONFIG_FILENAME = 'local.yml';
 
-export default () => yaml.load(
-  readFileSync(join(resolve('config'), YAML_CONFIG_FILENAME), 'utf8'),
-) as Record<string, any>;
+export default () => {
+  const defaults = yaml.load(
+    readFileSync(join(resolve('config'), YAML_DEFAULT_CONFIG_FILENAME), 'utf8'),
+  ) as Record<string, any>;
+
+  const overrideConfigPath = join(resolve('config'), YAML_OVERRIDE_CONFIG_FILENAME);
+  if (!existsSync(overrideConfigPath)) return defaults;
+
+  const overrides = yaml.load(
+    readFileSync(overrideConfigPath, 'utf8'),
+  ) as Record<string, any>;
+
+  return { ...defaults, ...overrides };
+}
